af3dfaae5f4a220bd9ee5db2c60f9ee8bdbf92d4
Can Mert Bozkurt <admin@talemon.com>
2024-01-27 19:40:18 +0100

    Began desktop project

---
+++ perform-desktop/Data/GameData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perform_desktop.Data
+{
+    public record struct IntRange
+    {
+        public int Min;
+        public int Max;
+    }
+
+    public record struct KeyAmount
+    {
+        public string Key;
+        public int Amount;
+    }
+
+    public class StatData
+    {
+        public string Key = string.Empty;
+        public string Name = string.Empty;
+    }
+
+    public class TokenData
+    {
+        public string Key = string.Empty;
+        public string Name = string.Empty;
+    }
+
+    public class MoveData
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public float SuccessChance { get; set; }
+
+        public KeyAmount[] StatRequirements { get; set; } = [];
+        public KeyAmount[] ItemRequirements { get; set; } = [];
+
+        public KeyAmount[] ItemBenefits { get; set; } = [];
+        public KeyAmount[] StatBenefits { get; set; } = [];
+
+        public KeyAmount[] FailureItemEffects { get; set; } = [];
+        public KeyAmount[] FailureStatEffects { get; set; } = [];
+
+        public IntRange Score { get; set; }
+        public int ActionPoints { get; set; }
+    }
+
+    public class GameData
+    {
+        public MoveData[] Moves = [];
+        public StatData[] Stats = [];
+        public TokenData[] Tokens = [];
+    }
+}
---
+++ perform-desktop/MainViewModel.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Newtonsoft.Json;
+using System.Windows.Input;
+using perform_desktop.Data;
+
+namespace perform_desktop
+{
+    public class StatData
+    {
+        public string Name { get; set; }
+    }
+    public class ItemData
+    {
+        public string Name { get; set; }
+    }
+
+    public class StatValue
+    {
+        public string StatKey { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class InventoryItem
+    {
+        public ItemData Item { get; set; }
+        public int Amount { get; set; }
+    }
+
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int _score;
+        private List<MoveData> _moves = [];
+        private int _laughter;
+        private int _confusion;
+        private int _attention;
+        private string _logContent = string.Empty;
+
+        public MainViewModel()
+        {
+            const string gamedataJson = "gamedata.json";
+            if (File.Exists(gamedataJson))
+            {
+                var gameData = JsonConvert.DeserializeObject<GameData>(File.ReadAllText(gamedataJson));
+
+                if (gameData != null)
+                {
+                    Moves = [..gameData.Moves];
+                }
+            }
+        }
+
+        public List<MoveData> Moves
+        {
+            get => _moves;
+            set
+            {
+                if (Equals(value, _moves)) return;
+                _moves = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Score
+        {
+            get => _score;
+            set
+            {
+                if (value == _score) return;
+                _score = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Laughter
+        {
+            get => _laughter;
+            set
+            {
+                if (value == _laughter) return;
+                _laughter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Confusion
+        {
+            get => _confusion;
+            set
+            {
+                if (value == _confusion) return;
+                _confusion = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Attention
+        {
+            get => _attention;
+            set
+            {
+                if (value == _attention) return;
+                _attention = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LogContent
+        {
+            get => _logContent;
+            set
+            {
+                if (value == _logContent) return;
+                _logContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
